1e3003987b874b96688d3cd67a82596c39973509
vinohts <vino.hts@gmail.com>
2026-07-18 20:21:33 +0530

    ec2.tf

--- terraform/environments/dev/ec2.tf
+++ terraform/environments/dev/ec2.tf
@@ -1,36 +1,36 @@
-# /*
-# ==============================================================================
-# KVS Platform
-# Application EC2 Instance
-# ==============================================================================
-# */
+/*
+==============================================================================
+KVS Platform
+Application EC2 Instance
+==============================================================================
+*/
 
-# resource "aws_instance" "app" {
+resource "aws_instance" "app" {
 
-#   ami = data.aws_ami.amazon_linux_2023.id
+  ami = data.aws_ami.amazon_linux_2023.id
 
-#   instance_type = "t3.small"
+  instance_type = "t3.small"
 
-#   subnet_id = aws_subnet.public_a.id
+  subnet_id = aws_subnet.public_a.id
 
-#   vpc_security_group_ids = [
-#     aws_security_group.app.id
-#   ]
+  vpc_security_group_ids = [
+    aws_security_group.app.id
+  ]
 
-#   iam_instance_profile = aws_iam_instance_profile.ec2.name
+  iam_instance_profile = aws_iam_instance_profile.ec2.name
 
-#   associate_public_ip_address = true
+  associate_public_ip_address = true
 
-#   tags = merge(
+  tags = merge(
 
-#     local.common_tags,
+    local.common_tags,
 
-#     {
+    {
 
-#       Name = "${local.project_name}-${var.environment}-app-01"
+      Name = "${local.project_name}-${var.environment}-app-01"
 
-#     }
+    }
 
-#   )
+  )
 
-# }
+}
